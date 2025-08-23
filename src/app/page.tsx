f15b3b667390660cdd4a49e1d408603a2401a089
KloseBH <danielhabdias@gmail.com>
2025-08-22 22:23:14 -0300

    #0003 - Feature: Criado banner com fundo em video.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import ClientHeader from "@/components/ClientHeader";
+import Banner from "@/components/Banner";
 
 export default function Home() {
+  
   return (
     <div>
       <ClientHeader name={'Daniel'}/>
+      <Banner name={'Daniel Dias'}/>
     </div>
   );
 }
---
+++ src/components/Banner.tsx
@@ -0,0 +1,48 @@
+import Box from '@mui/material/Box';
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
+import '@/../public/glitch.css';
+
+export default function Banner({name} : {name: string}) {
+  return (
+    <Box sx={{ position: "fixed", width: "100%", height: "100vh", overflow: "hidden" , top: 0}}>
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        style={{
+          position: "absolute",
+          width: "100%",
+          height: "100%",
+          objectFit: "cover",
+          top: 0,
+          left: 0,
+          zIndex: -1,
+        }}
+      >
+        <source src="/banner.mp4" type="video/mp4" />
+        Seu navegador não suporta vídeo HTML5.
+      </video>
+
+      <Box
+        sx={{
+          position: "relative",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100%",
+          color: "white",
+          textAlign: "center",
+          flexDirection: "column",
+          flexWrap: "wrap"
+        }}
+      >
+        <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" sx={{ width: 200, height: 200}}/>
+        <Typography variant="h3" component="div">
+            {name}
+        </Typography>
+      </Box>
+    </Box>
+  );
+}
